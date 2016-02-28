19cffbc007a4d96f27f628bd2c4c719d6d38b86b
Ose, Alexander <null>
2016-02-28 10:43:39 -0500

    move files around

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ var app = express();
 var config = require('./config.js');
 var spreadsheet = require('./spreadsheet.js');
 
+app.use(express.static('../client'));
 
 app.get('/api/fetch/:id', function (req, res){
   var id = request.params.id;
@@ -16,5 +17,5 @@ app.get('/api/search/:id', function (req, res){
   res.send('Hello World!');
 });
 
-app.listen(3000, function (){
+app.listen(3000, function(){
 });

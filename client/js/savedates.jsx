c1fd4ab04f1a443b801af056f4c7322e2f3020e3
Ose, Alexander <null>
2016-03-17 22:17:12 -0400

    building out save the dates

--- client/js/savedates.jsx
+++ client/js/savedates.jsx
@@ -1,35 +1,98 @@
 var React = require('react');
+var request = require('superagent');
 
-var SaveDate = React.createClass({
+var Front = React.createClass({
   getInitalState : function(){
     console.log('do stuff');
   },
   render : function(){
     return(
-      <div class="savedate">
-        <div class="front">
-          <h1>Maluhia and Alex</h1>
-          <div class="left">
-            <p>Applegate, Oregon</p>
-            <date>10.08.2016</date>
-          </div>
-          <div class="right">
-            <p>Kea'au, Hawaii</p>
-            <date>11.26.2016</date>
-          </div>
+      <div className="savedate front">
+        <h1>Maluhia and Alex</h1>
+        <div className="left">
+          <p>Applegate, Oregon</p>
+          <date>10.08.2016</date>
         </div>
-        <div class="back">
-          <h1>Save the Date</h1>
-          <p>Wedding in Applegate, Oregon</p>
-          <date>October 8, 2016</date>
-          <p>Reception in Kea’au, Hawaii</p>
-          <date>November 26, 2016</date>
-          <p>Formal invitation to follow</p>
-          <p>wedding.farm/RSVP/HL3C</p>
+        <div className="right">
+          <p>Kea'au, Hawaii</p>
+          <date>11.26.2016</date>
         </div>
       </div>
     )
   }
 });
 
+var Back = React.createClass({
+  getInitalState : function(){
+  },
+  render : function(){
+    var card = this.props.card;
+    return(
+      <div className="savedate back">
+        <div className="left">
+          <h1>Save the Date</h1>
+          <p>Wedding in Applegate, Oregon</p>
+          <date>October 8, 2016</date>
+          <p>Reception in Kea’au, Hawaii</p>
+          <date>November 26, 2016</date>
+          <p>Formal invitation to follow</p>
+          <p>wedding.farm/RSVP/{card.code}</p>
+        </div>
+        <div className="right">
+          <div className="stamp">&nbsp;</div>
+          <div>
+            {card['Mail Name 1']} <br />
+            {card['Mail Name 2']} <br />
+            {card['Address 1']} <br />  
+            {card['Address 2']} <br />
+            {card['City']} {card['State']} {card['Zip Code']}<br />
+            {card['Country']} <br />
+          </div>
+        </div>
+      </div>
+    )
+  }
+});
 
+var Container = React.createClass({
+  getInitialState: function(){
+    request
+      .get('/api/invitation/')
+      .set('Accept', 'application/json')
+      .end(function(err, data){
+        var json = JSON.parse(data.text);
+
+        var arr = [];
+        for (var i in json){
+          arr.push(json[i]);
+        }
+
+        this.setState({ cards : arr }); 
+      }.bind(this));
+
+    return { cards : [] };
+  },
+  render : function(){
+    var cards = this.state.cards;
+    return(
+      <div className="savedate-container">
+        {
+          cards.map(function(d){
+            return (
+              <Back card={d} />
+            )
+          })
+        }
+        {
+          cards.map(function(d){
+            return (
+              <Front />
+            )
+          })
+        }
+      </div>
+    )
+  }
+});
+
+module.exports = Container;

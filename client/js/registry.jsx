14c6e67aab30c7f086f6a4cedfe7eca51908c9c1
Alexander Ose <alex@alexose.com>
2016-09-02 10:16:47 -0700

    mobile fixes

--- client/js/registry.jsx
+++ client/js/registry.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var Registry = React.createClass({
   render : function(){
     return(
-      <div className={'page centered'}>
+      <div className='page centered'>
         <h1 className="title"><img width="500px" src="img/registry.png" /></h1>
         <p>No gifts, please!</p>
         <p>But if you can't resist, you may buy us a plant.</p>

c665413ec8f6e8c7c22c86cae2cf2aeb5268ec53
Alexander Ose <alex@alexose.com>
2016-09-10 23:38:17 -0700

    add registry link

--- client/js/registry.jsx
+++ client/js/registry.jsx
@@ -6,8 +6,7 @@ var Registry = React.createClass({
       <div className={'page centered'}>
         <h1 className="title"><img width="500px" src="img/registry.png" /></h1>
         <p>No gifts, please!</p>
-        <p>But if you can't resist, you may buy us a plant.</p>
-        <p>(We will be registered at the <a href="http://www.applegatenursery.com/">Applegate Nursery</a>.  More information soon!)</p>
+        <p>But if you can't resist, <a href="https://www.weddingrepublic.com/weddingdotfarm">you may buy us a plant</a>.</p>
       </div>
     )
   }
